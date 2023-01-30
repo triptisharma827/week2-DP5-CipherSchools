f0c8e6f7ef722e1f23ef7ca7a28cd4e262a72b2b
triptisharma827 <triptisharma827@gmail.com>
2023-01-31 02:50:01 +0530

    added next greater/smaller element problem

---
+++ nextGreaterEleII.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        vector<int> ans(nums.size());
+
+        for(int i=nums.size()-1 ; i>=0 ; i--)
+        {
+            st.push(nums[i]);
+        }
+
+        for(int i=nums.size()-1 ; i>=0 ; i--)
+        {
+            while(!st.empty() && nums[i]>=st.top())
+            {
+                st.pop();
+            }
+
+            if(!st.empty())
+            {
+                ans[i]=(st.top());
+            }
+            else
+            {
+                ans[i]=(-1);
+            }
+            
+            st.push(nums[i]);
+        }
+
+        // reverse(ans.begin() , ans.end());
+
+        return ans;
+    }
+};
